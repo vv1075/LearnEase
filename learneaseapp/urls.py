0289370492aa6bcadc693c8db36900ab72f741ef
VinodMamillapalli <vm1147@usnh.edu>
2024-04-16 19:17:45 -0400

    create login for learnease

--- learneaseapp/urls.py
+++ learneaseapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('login/', auth_views.LoginView.as_view(template_name = "learneaseapp/login.html"),name='login'),
     
 ] 
