48eec6bb8bc4dd16426f6dc01ce1503f9b335507
VinodMamillapalli <vm1147@usnh.edu>
2024-05-14 00:58:02 -0400

    Create a middleware file and Add code for middleware for app

--- learnease/settings.py
+++ learnease/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.block_mobile_middleware.BlockMobileMiddleware',
 ]
 
 ROOT_URLCONF = 'learnease.urls'
